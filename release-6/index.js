fb4d201adac96c9ed5d12c169ecd6f2a82cae40e
Alekots <ffddgg.79@mail.ru>
2021-11-05 15:56:10 +0300

    1st6 git commit -m 1st6 git push clear

--- release-6/index.js
+++ release-6/index.js
@@ -1,4 +1,8 @@
 // 7 - > 13
-const fib = () => {
-    // code here
+const fib = (n) => {
+ if (n == 1) {return 1} else{
+ if (n == 2){ return 1} else{
+return (fib(n-2) + fib(n-1))
+ }}
 }
+console.log(fib(7))
