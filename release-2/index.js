5aa4020bec2fbe19879ec5b44f5d474e14acb97a
Elisabeth <lisaveta.gl@yandex.ru>
2021-11-06 16:28:52 +0300

    Other half of release-2

--- release-2/index.js
+++ release-2/index.js
@@ -2,24 +2,12 @@
 // [1, 5, 2], 1 -> [1, 2, 5]
 
 const sorting = (arr, direction) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (direction === 1) { 
-            if (arr[i + 1] <= arr[i]) {
-                let temp = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i + 1] = temp
-            }
-        } else if (direction === -1) {
-            if (arr[i + 1] >= arr[i]) {
-                let temp = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i + 1] = temp
-            }
-        } else {
-            return 'Incorrect data was given'
-        }
+    if (direction === -1) {
+        return arr.sort().reverse()
+    } else if (direction != (1 || -1)) {
+        return 'Incorrect data was given'
     }
-    return arr
+    return arr.sort()
 }
 
 console.log(sorting([1, 5, 2], -1))
@@ -27,34 +15,38 @@ console.log(sorting([1, 5, 2], 1))
 console.log(sorting([1, 5, 2], 0))
 
 // [{age: 15, height: 140}, {age: 14, height: 150}], age -> [{age: 14, height: 150}, {age: 15, height: 150}]
-const deepSorting = () => {
-    //code here
+const deepSorting = (objArr, key) => {
+    objArr.sort((from, to) => (from[key] > to[key] ? 1 : -1))
+    return objArr
 }
 
-
-
+console.log(deepSorting([{age: 15, height: 140}, {age: 14, height: 150}], 'age'))
 
 // ['d', 1, 3, null] -> [1, 3]
-const getNumbers = () => {
-    //code here
+const getNumbers = (arr) => {
+    let sortedArr = []
+    for (let i = 0; i < arr.length; i++) {
+        typeof(arr[i]) === 'number' ? sortedArr.push(arr[i]) : 0
+    }
+    return sortedArr
 }
 
-
-
+console.log(getNumbers(['dick', 1, 3, null]))
 
 // [1, 5, 34, -100] -> -100
-const min = () => {
-    //code here
+const min = (arr) => {
+    arr.sort()
+    return arr[0]
 }
 
-
+console.log(min([1, 5, 34, -100]))
 
 // [1, 1, 1, 3, 4, 2, 2] -> [1, 3, 4, 2]
-const getSet = () => {
-    //code here
+const getSet = (arr) => {
+    return [...new Set(arr)]
 }
 
-
+console.log(getSet([1, 1, 1, 3, 4, 2, 2]))
 
 // // [1, 1, 1, 3, 4, 2, 2, 2] -> [1, 2]
 const findTheMostReapetedEls = () => {
