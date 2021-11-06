d83d50b56a5c1ad7381d495421f74ad31310cf06
lieless <kadet14022002@mail.ru>
2021-11-06 18:37:12 +0300

    release 1

--- release-1/index.js
+++ release-1/index.js
@@ -1,30 +1,30 @@
 //  'Nikita' -> 'Hi, Nikita'
- const hiFunction = () => {
-    //code here
+ const hiFunction = (name) => {
+   return ("Hi," + name);
 }
 
 
 // 5, 6 -> 11
- const sum = () => {
-    //code here
+ const sum = (a, b) => {
+    return (a+b);
 }
 
 
 //5 -> false
 //0 -> true
 //4 -> true
- const isEven = () => {
-    //code here
+ const isEven = (number) => {
+    return(Boolean(number % 2));
 }
 
 
 // 2 -> i have 2 apples
- const apples = () => {
-    //code here
+ const apples = (entity) => {
+    return("i have " + " " + entity + " " + "apples")
 }
 // 4 -> 16
- const getPower = () => {
-    //code here
+ const getPower = (number) => {
+    return(number*number)
 }
 
 module.exports = {
---
+++ tests.js
@@ -0,0 +1,31 @@
+// //  'Nikita' -> 'Hi, Nikita'
+// var a = "Nikita"
+
+// const hiFunction = (name) => {
+//     return ("Hi," + " " + name);
+//  }
+
+// var a = 77, b = 71
+// const sum = (a, b) => {
+//     return (a+b)
+// }
+// console.log(sum(a, b))
+
+// const isEven = (number) => {
+//     return(Boolean(!(number % 2)));
+// }
+// console.log(isEven(-5))
+ 
+// 2 -> i have 2 apples
+
+// const apples = (entity) => {
+//     return("i have" + " " + entity + " " + "apples")
+// }
+// console.log(apples(111))
+
+// // 4 -> 16
+// const getPower = (number) => {
+//     return(number*number)
+// }
+// console.log(getPower(5.5))
+ 
