d3a05f8807de4163132d83468387c56d18554cf2
MyName <lina.ibragimova26@gmail.com>
2021-11-06 18:37:53 +0300

    release-1 done

--- release-1/index.js
+++ release-1/index.js
@@ -1,31 +1,36 @@
 //  'Nikita' -> 'Hi, Nikita'
- const hiFunction = () => {
-    //code here
+var a="Nikita"
+ const hiFunction = (name) => {
+    return ('Hi,' + name)
 }
 
 
 // 5, 6 -> 11
- const sum = () => {
-    //code here
+
+var m = 5, n = 6
+ const sum = (m, n) => {
+    return (m+n)
 }
 
 
 //5 -> false
 //0 -> true
 //4 -> true
- const isEven = () => {
-    //code here
+ const isEven = (num) => {
+    return(Boolean(!(num % 2)))
 }
 
 
 // 2 -> i have 2 apples
- const apples = () => {
-    //code here
+ const apples = (num) => {
+    return("i have " + num + " apples")
 }
+
 // 4 -> 16
- const getPower = () => {
-    //code here
+ const getPower = (num) => {
+    return(num*num)
 }
+
 
 module.exports = {
     hiFunction,
