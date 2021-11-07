6bcc377900d2d84b797728132753924eb6bfe57d
MyName <lina.ibragimova26@gmail.com>
2021-11-08 00:02:07 +0300

    release-2, release-5, release-6 done

--- release-6/index.js
+++ release-6/index.js
@@ -1,4 +1,8 @@
 // 7 - > 13
-const fib = () => {
-    // code here
+const fib = (n) => {
+    if (n===0) return 0
+    if (n===1) return 1
+    if (n<0) return ("Sorry, this is cringe. Please, try again")
+    return fib(n-1) + fib(n-2)
 }
+console.log(fib(7))
