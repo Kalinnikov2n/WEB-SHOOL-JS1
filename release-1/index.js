24875b3048b6e15cad571b52d0ccb64fa131bf9b
Zinkyufox <wwwzink@mail.ru>
2021-11-07 11:55:30 +0300

    Решен release-1

--- release-1/index.js
+++ release-1/index.js
@@ -1,30 +1,34 @@
 //  'Nikita' -> 'Hi, Nikita'
- const hiFunction = () => {
-    //code here
+ const hiFunction = (name) => {
+    return "Hi, " + name
 }
 
 
 // 5, 6 -> 11
- const sum = () => {
-    //code here
+ const sum = (num1,num2) => {
+    return num1+num2
 }
 
 
 //5 -> false
 //0 -> true
 //4 -> true
- const isEven = () => {
-    //code here
+ const isEven = (num) => {
+    if(num%2!==0){
+        return false
+    }else{
+        return true
+    }
 }
 
 
 // 2 -> i have 2 apples
- const apples = () => {
-    //code here
+ const apples = (appleCount) => {
+    return "i have " + appleCount + " apples"
 }
 // 4 -> 16
- const getPower = () => {
-    //code here
+ const getPower = (num) => {
+    return Math.pow(num,2);
 }
 
 module.exports = {
