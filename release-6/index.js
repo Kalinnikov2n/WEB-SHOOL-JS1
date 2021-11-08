bc78e3fb3c167e9340a8dc9544e043ba7614b145
Zinkyufox <wwwzink@mail.ru>
2021-11-08 13:49:43 +0300

    Решен release-6

--- release-6/index.js
+++ release-6/index.js
@@ -1,4 +1,8 @@
 // 7 - > 13
-const fib = () => {
-    // code here
+const fib = (arg) => {
+    if(arg==1 || arg==2){
+        return 1
+    }else{
+        return fib(arg-2) + fib(arg-1);
+    }
 }
