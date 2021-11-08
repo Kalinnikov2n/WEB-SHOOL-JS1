aa62afb589e8122efae7b15aea284b9e1434bd44
Zinkyufox <wwwzink@mail.ru>
2021-11-08 13:34:40 +0300

    Решен release-5

--- release-5/index.js
+++ release-5/index.js
@@ -1,5 +1,51 @@
 //'e3' -> ['d1', 'c2', 'c4', 'd5', 'f5', 'g4', 'g2', 'f1'] 
 
-export const horseSteps = () => {
-    //code here
+const horseSteps = (pos) => {
+    let str= " abcdefghij" //h -конец доски
+    let coord = pos.split("");
+    let turns = [];
+    let turn = "";
+    coord[0]=str.indexOf(coord[0])
+    coord[1]=Number(coord[1])
+    if(coord[0]+1<=8){
+        if(coord[1]+2<=8){
+            turn = str[coord[0]+1]+(coord[1]+2);
+            turns.push(turn);
+        }
+        if(coord[1]-2>=1){
+            turn = str[coord[0]+1]+(coord[1]-2);
+            turns.push(turn);
+        }
+        if(coord[0]+2<=8){
+            if(coord[1]+1<=8){
+                turn = str[coord[0]+2]+(coord[1]+1);
+                turns.push(turn);
+            }
+            if(coord[1]-1>=1){
+                turn = str[coord[0]+2]+(coord[1]-1);
+                turns.push(turn);
+            }
+        }
+    }
+    if(coord[0]-1<=8){
+        if(coord[1]+2<=8){
+            turn = str[coord[0]-1]+(coord[1]+2);
+            turns.push(turn);
+        }
+        if(coord[1]-2>=1){
+            turn = str[coord[0]-1]+(coord[1]-2);
+            turns.push(turn);
+        }
+        if(coord[0]-2<=8){
+            if(coord[1]+1<=8){
+                turn = str[coord[0]-2]+(coord[1]+1);
+                turns.push(turn);
+            }
+            if(coord[1]-1>=1){
+                turn = str[coord[0]-2]+(coord[1]-1);
+                turns.push(turn);
+            }
+        }
+    }
+    console.log(turns)
 }
