bb9fadc73f019aaa1d9a2bb3987b0a60200a556f
danilbazhitov <danil.bazhitov123@mail.ru>
2021-11-09 19:28:44 +0300

    1/2 release-2

--- release-2/index.js
+++ release-2/index.js
@@ -1,75 +1,134 @@
 // [1,5, 2], -1 -> [5, 2, 1]
 // [1, 5, 2], 1 -> [1, 2, 5]
 
-export const sorting = () => {
-    //code here
+const sorting = (a,x) => {
+    var b=a.sort();
+    if (x==1){
+        console.log(b)
+    } else{
+        console.log(b.reverse())
+    }
 }
-
+// sorting([1,2,3],1)
 // [{age: 15, height: 140}, {age: 14, height: 150}], age -> [{age: 14, height: 150}, {age: 15, height: 150}]
-export const deepSorting = () => {
-    //code here
+const deepSorting = (da,l) => {
+    if (l=="height"){
+        console.log(da.sort((a,b) =>  a.height-b.height ))
+    } else{
+        console.log(da.sort((a,b) =>  a.age-b.age ))
+    }
 }
+// deepSorting([{"age": 15, "height": 140}, {"age": 14, "height": 150},{"age": 13, "height": 180}], "age")
 
 
 
 
 // ['d', 1, 3, null] -> [1, 3]
-export const getNumbers = () => {
-    //code here
+const getNumbers = (array) => {
+    array = array.filter(item => typeof item === "number");
+    console.log(array);
 }
+// getNumbers(['d', 1, 3, null])
 
 
 
 
 // [1, 5, 34, -100] -> -100
-export const min = () => {
-    //code here
+const min = (arr) => {
+    console.log(Math.min(...arr))
 }
-
+// min([1, 5, 34, -100])
 
 
 // [1, 1, 1, 3, 4, 2, 2] -> [1, 3, 4, 2]
-export const getSet = () => {
-    //code here
-}
+const getSet = (arr) => {
+    var i,
+      len = arr.length,
+      out = [],
+      obj = {};
 
+  for (i = 0; i < len; i++) {
+    obj[arr[i]] = 0;
+  }
+  for (i in obj) {
+    out.push(i);
+  }
+  console.log(out)
+}
+// getSet([1, 1, 1, 3, 4, 2, 2])
 
 
 // // [1, 1, 1, 3, 4, 2, 2, 2] -> [1, 2]
-export const findTheMostReapetedEls = () => {
-    //code here
+const findTheMostReapetedEls = (names) => {
+    var uniq = names
+    .map((name) => {
+      return {
+        count: 1,
+        name: name
+      }
+    })
+    .reduce((a, b) => {
+      a[b.name] = (a[b.name] || 0) + b.count
+      return a
+    }, {})
+  
+  var duplicates = Object.keys(uniq).filter((a) => uniq[a] > 1)
+  
+  console.log(duplicates)
 }
+// findTheMostReapetedEls([1, 1, 1, 3, 4, 2, 2, 2])
 
 
 // ['[','[', ']', ']' ] -> true
 // [']', '[', ']'] -> false
-export const stack = () => {
-    //code here
+const stack = (arr) => {
+    var c =0;
+    var m=0;
+    if (arr.length%2==0 && arr[0]=='['){
+        for(let i=0;i<arr.length;i++){
+            if (arr[i]=='['){
+                c+=1
+            } else{
+                m+=1
+                if ((m+c)%2!=0){
+                    c=-1
+                    break
+                }
+            }
+        }
+    } else{
+        c=-1
+    }
+    if (c==m){
+        console.log(true)
+    } else{
+        console.log(false)
+    }
 }
-
+// stack(['[',']','[',']','[','['])
 
 
 // 'Hi, Nikita', 'Hi' -> true
-export const checkForBadWord = () => {
+const checkForBadWord = () => {
     //code here
 }
 
-export const customSome = () => {
+const customSome = () => {
     //code here
 }
 
-export const customFind = () => {
+const customFind = () => {
 //code here
 }
 
-export const customMap = () => {
+const customMap = () => {
     //code here
 }
 
-export const customReducer = () => {
+const customReducer = () => {
     //code here
 }
 
-export const custonFilter = () => {
+const custonFilter = () => {
     //code here
 }
