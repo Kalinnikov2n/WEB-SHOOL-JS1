2e369c15d26a3d99efde9ebd693d6a2c90ffae7d
Empty-string2710 <ankalmykova@gmail.com>
2021-11-11 17:52:55 +0300

    release-1

--- release-1/index.js
+++ release-1/index.js
@@ -1,36 +1,47 @@
 //  'Nikita' -> 'Hi, Nikita'
- const hiFunction = () => {
-    //code here
+const hiFunction = (name) => {
+    console.log("Hi", name)
 }
 
 
 // 5, 6 -> 11
- const sum = () => {
-    //code here
+ const sum = (a,b) => {
+    console.log(a+b)
 }
 
 
 //5 -> false
 //0 -> true
 //4 -> true
- const isEven = () => {
-    //code here
+ const isEven = (a) => {
+    if (a%2 == 0) {
+        console.log(true)
+    }
+    else{
+        console.log(false)
+    }
 }
 
 
 // 2 -> i have 2 apples
- const apples = () => {
-    //code here
+ const apples = (n) => {
+    if (n!=1) {
+        console.log("I have " + n + " apples")
+    }
+    else {
+        console.log("I have " + n + " apple")
+    }
 }
 // 4 -> 16
- const getPower = () => {
-    //code here
+ const getPower = (n) => {
+    console.log(n*n)
 }
 
-module.exports = {
-    hiFunction,
-    sum,
-    isEven,
-    apples,
-    getPower,
-}
+hiFunction("Anna")
+sum(2,3)
+isEven(4)
+isEven(5)
+apples(4)
+apples(1)
+getPower(5)
+
