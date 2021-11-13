bf76b4054a5c938806d55a673f325ef35fcf9481
Elisabeth <lisaveta.gl@yandex.ru>
2021-11-13 19:06:08 +0300

    some changes

--- release-6/index.js
+++ release-6/index.js
@@ -1,4 +1,12 @@
 // 7 - > 13
-const fib = () => {
-    // code here
+const fib = (index) => {
+    var fibonacciArr = [1, 1]
+    for (let i = 2; i < index + 1; i++) {
+        fibonacciArr.push(fibonacciArr[i -1] + fibonacciArr[i-2])
+    }
+    return fibonacciArr[index - 1]
 }
+
+// Это считается за рекурсию?
+
+console.log(fib(7))
