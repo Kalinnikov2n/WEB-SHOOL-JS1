1375c502d8d781a5d07ee47d9afd525cf1d0282b
Elizaveta <lizbashir@gmail.com>
2021-11-17 00:32:36 +0300

    release-1 + release-6 complete

--- release-1/index.js
+++ release-1/index.js
@@ -1,30 +1,36 @@
 //  'Nikita' -> 'Hi, Nikita'
- const hiFunction = () => {
-    //code here
+ const hiFunction = (name) => {
+    console.log("Hi, " + name);
 }
 
 
 // 5, 6 -> 11
- const sum = () => {
-    //code here
+ const sum = (num1, num2) => {
+    console.log(num1 + num2);
 }
 
 
 //5 -> false
 //0 -> true
 //4 -> true
- const isEven = () => {
-    //code here
+ const isEven = (num) => {
+    if (a%2===0){
+        return true
+    } else{
+        return false
+    }
 }
 
 
 // 2 -> i have 2 apples
- const apples = () => {
-    //code here
+ const apples = (number) => {
+    console.log("i have" + number + "apples");
 }
+
+
 // 4 -> 16
- const getPower = () => {
-    //code here
+ const getPower = (num) => {
+    console.log(Math.pow(num, 2));
 }
 
 module.exports = {
--- release-6/index.js
+++ release-6/index.js
@@ -1,4 +1,5 @@
 // 7 - > 13
-const fib = () => {
-    // code here
+const fib = (index) => {
+    return (index == 1) ? index : fib(index - 1) + fib(index - 2);
 }
+console.log(fib(16))
