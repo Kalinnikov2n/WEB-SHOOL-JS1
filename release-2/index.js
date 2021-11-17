573e5a582ed38389949e5b8ddc782ea46f4f49a8
danilbazhitov <danil.bazhitov123@mail.ru>
2021-11-17 17:25:39 +0300

    чето баг с 3м релизом

--- release-2/index.js
+++ release-2/index.js
@@ -138,14 +138,14 @@ const customMap = (a) => {
 
 const customReducer = (a) => {
     for(let i=0;i<a.length;i++){
-        if (a[i]==a[-1]){
+        if (a[i]==a[a.length-1]){
             a[i]=a[i]+a[i-1]
             break
         } else{
             a[i]+=a[i+1]
         }
     }
-    return a[-1]
+    return a[a.length-1]
 }
 
 const custonFilter = (a) => {
--- release-4/index.js
+++ release-4/index.js
@@ -1,16 +1,34 @@
 
-export const calc = () => {
-    //code here
+const calc = (a) => {
+    var c=""
+    c=c.concat(a)
+    console.log(Number(c))
+    if(a=="+"){
+        console.log(myCalc+a)
+    }
 }
-
-// const myCalc = calc()
+// myCalc(5)
+// myCalc("+")
+// myCalc(1)
+// const myCalc = calc
 // myCalc(5)
 // myCalc('+')
 // myCalc(3)
 // myCalc('/')
 // myCalc(4)
 // 2
-
-export const useMemo = () => {
-    //code here
+var m=0;
+var d=0;
+const getPower = (a) => {
+    console.log(a*a);
 }
+const useMemo = (c) => {
+    d=c(a)
+    if (d==m){
+        console.log("hduwedhcuwecvuwe")
+    }
+    m=c(a)
+}
+const func = useMemo(getPower)
+func(1)
+func(1)
