a2b0d80a6d39eee443878fdd83f2857e623e9a50
VeeperL <tt.o.p@yandex.ru>
2021-11-17 11:10:00 +0300

    release-2

--- release-2/index.js
+++ release-2/index.js
@@ -1,75 +1,131 @@
 // [1,5, 2], -1 -> [5, 2, 1]
 // [1, 5, 2], 1 -> [1, 2, 5]
 
-export const sorting = () => {
-    //code here
-}
+
+ const sorting = (mas,x) => {
+     var n = mas.sort()
+  if (x==1) { 
+      console.log(n)
+  }
+else{
+    console.log(n.reverse())
+ }
+  }
+sorting([1,5,2],-1)
+
+
 
 // [{age: 15, height: 140}, {age: 14, height: 150}], age -> [{age: 14, height: 150}, {age: 15, height: 150}]
-export const deepSorting = () => {
-    //code here
-}
-
-
+ const deepSorting = (users) => {
+    users.sort((prev, next) => prev.age - next.age)
+    console.log(users)
+ }
+deepSorting([{age: 15, height: 140}, {age: 14, height: 150}])
 
 
 // ['d', 1, 3, null] -> [1, 3]
-export const getNumbers = () => {
-    //code here
+const getNumbers = (mas) => {
+   console.log(mas.slice(1,3)) //code here
 }
+getNumbers(['d', 1 , 3, null])
+
 
 
 
 
 // [1, 5, 34, -100] -> -100
-export const min = () => {
-    //code here
-}
+const min = (mas) => {
+  let min = mas[0]
+  for( let i=0; i< mas.length; i++) { 
+  if (mas[0]>mas[i]) min=mas[i] 
+} console.log(min) }
+min([1,5,34,-100])
 
 
 
 // [1, 1, 1, 3, 4, 2, 2] -> [1, 3, 4, 2]
-export const getSet = () => {
-    //code here
-}
 
+   const getSet = (mas) => {
+var n = []
+for (let i = 0; i<mas.length; i++) {
+   if (mas.indexOf(mas[i])==-1) {
+      mas.push(mas[i])
+   }
+}
+console.log(mas)
+   }
+getSet([1,1,1,3,4,2,2])
 
 
 // // [1, 1, 1, 3, 4, 2, 2, 2] -> [1, 2]
-export const findTheMostReapetedEls = () => {
+ const findTheMostReapetedEls = () => {
     //code here
 }
 
 
 // ['[','[', ']', ']' ] -> true
 // [']', '[', ']'] -> false
-export const stack = () => {
-    //code here
+ const stack = () => {
+   if (mas.filter(x => x=="]").length == mas.filter(x => x=="[").length){
+      console.log(true)
+  }
+  else {
+      console.log(false) //code here
 }
 
 
 
 // 'Hi, Nikita', 'Hi' -> true
-export const checkForBadWord = () => {
+ const checkForBadWord = (stroka,slovo) => {
+   
+   
+console.log(stroka.includes(slovo))
+
+}
+checkForBadWord("Hi Nikita", "Hi")
+
+
+
+ const customSome = () => {
+   let n = 0
+   for (let i = 0; i < mas.length; i++ ){
+       if (mas[i] % 2 == 0){
+           console.log(true)
+           n += 1
+           break
+       }
+   if (n == 0){
+       console.log(false)
+   }
+   }
+}
+customSome([1,2,1,1,1,1])  //code here
+}
+
+ 
+
+
+
+
+
+
+ const customMap = () => {
+  const customFind = (mas) => {
+   for (let i = 0; i < mas.length; i++ ){
+      if (mas[i] % 2 == 0){
+          console.log(mas[i])
+          break
+      }
+}  
+}
+customFind([1,2,1,1,1,1])
+
+
+
+ const customReducer = () => {
     //code here
 }
 
-export const customSome = () => {
-    //code here
-}
-
-export const customFind = () => {
-//code here
-}
-
-export const customMap = () => {
-    //code here
-}
-
-export const customReducer = () => {
-    //code here
-}
-
-export const custonFilter = () => {
+ const custonFilter = () => {
     //code here
 }
