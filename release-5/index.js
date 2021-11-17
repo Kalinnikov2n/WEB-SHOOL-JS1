16ef95ca4f6d9d989055c08d59521d9d7cf4043f
VeeperL <tt.o.p@yandex.ru>
2021-11-17 11:28:57 +0300

    release 5

--- release-5/index.js
+++ release-5/index.js
@@ -1,5 +1,37 @@
 //'e3' -> ['d1', 'c2', 'c4', 'd5', 'f5', 'g4', 'g2', 'f1'] 
 
-export const horseSteps = () => {
-    //code here
+ const horseSteps = (posit) => {
+    for (let i = 0; i < 8; i++ ){
+        for (let j = 0; j < 8; j++ ){
+            if (arr[i][j] == posit){
+                // console.log(arr[i-2][j-1],arr[i-2][j+1],arr[i-1][j-2],arr[i-1][j+2],arr[i+1][j-2],arr[i+1][j+2],arr[i+2][j-1],arr[i+2][j+1])
+                if (typeof arr[i-2][j-1] != "undefined"){
+                    result.push(arr[i-2][j-1])
+                }
+                if (typeof arr[i-2][j+1] != "undefined"){
+                    result.push(arr[i-2][j+1])
+                }
+                if (typeof arr[i-1][j-2] != "undefined"){
+                    result.push(arr[i-1][j-2])
+                }
+                if (typeof arr[i-1][j+2] != "undefined"){
+                    result.push(arr[i-1][j+2])
+                }
+                if (typeof arr[i+1][j-2] != "undefined"){
+                    result.push(arr[i+1][j-2])
+                }
+                if (typeof arr[i+1][j+2] != "undefined"){
+                    result.push(arr[i+1][j+2])
+                }
+                if (typeof arr[i+2][j-1] != "undefined"){
+                    result.push(arr[i+2][j-1])
+                }
+                if (typeof arr[i+2][j+1] != "undefined"){
+                    result.push(arr[i+2][j+1])
+                }
+                console.log(result)
+            }
+        }
+    }
 }
+horseSteps("e3")
