976051dec50dc0a36c3aaec74b4bf66bd16eace7
MyName <lina.ibragimova26@gmail.com>
2021-11-17 18:24:37 +0300

    final homework here

--- release-3/index.js
+++ release-3/index.js
@@ -1,6 +1,6 @@
 // {name: 'Lim', surname: 'Bok', age: 12} -> 'Bok Lim'
-export const fullName = () => {
-    //code here
+const fullName = (obj) => {
+    return(obj.surname+" "+obj.name)
 }
 
 
@@ -8,16 +8,40 @@ export const fullName = () => {
 // {key: 5}, 'key' -> true
 // {key: null}, 'key' -> false
 
-export const checkForPropertyAndValue = () => {
-    //code here
+const checkForPropertyAndValue = (obj, key) => {
+    if(key in obj && obj[key]>0) return true
+    return false
 }
 
 // [{name: 'Vova', age: 12, type: 'owner'}, {type: 'computer', owner: 'Vova', name: 'computerName', oc: 'Mac'}, {type: 'phone', name: 'phoneName', owner: 'Vova', oc: 'ios'}] -> [{name: 'Vova', computers: [{ name: 'computerName', oc: 'Mac'}], phones: [{name: 'phoneName', oc: 'ios'}] }]
-export const parser = () => {
+const parser = () => {
     //code here
 }
+
 // '{name: 'Vova', age: 12, type: 'owner'}' -> {name: 'Vova', age: 12, type: 'owner'}
-export const createObjFromStr = () => {
-    //code here
-}
+const createObjFromStr = (str) => {
+        let strObj = {}, arrStr = [], newStr, key, value, counter = 0
+        if(str[0] != '{' || str.slice(-1) != '}') return ('ERROR')
+        newStr = str.slice(1, -1)
+        arrStr = newStr.split(', ')
+        for(el of arrStr) {
+           for(char of el) {
+                if(char==':') {
+                    counter++
+                }
+            }
+            if(counter!=1) {
+                return ("ERROR")
+            }
+            counter = 0
+        }
+        for(el of arrStr) {
+            if(!(el.includes(":"))) {
+                return ("ERROR")
+            }
+            [key, value] = el.split(": ")
+            strObj[key] = value.replace(/[^a-zа-яё0-9\s]/gi, '')
+        }
+        return strObj
+    }
 
